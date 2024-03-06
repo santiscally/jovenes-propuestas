a7c00923f9775d0149d52d6e651181320e76bcc0
unknown <santiscally@gmail.com>
2024-03-06 16:30:58 -0300

    primer commit

---
+++ back/src/main/java/com/jovenes/propuestas/JovenesPropuestasApplication.java
@@ -0,0 +1,13 @@
+package com.jovenes.propuestas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JovenesPropuestasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JovenesPropuestasApplication.class, args);
+	}
+
+}
